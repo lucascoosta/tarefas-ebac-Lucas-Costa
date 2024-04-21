c30c2ec4fd6efff6902f702811f4b3f09e6dd5cd
Lucas Costa <ls.coosta@hotmail.com>
2024-04-21 18:55:36 -0300

    Quinta tarefa

---
+++ mod9/TarefaWrapper/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    public static void main(String[] args) {
+        int num = 5;
+        Integer numWrapper = num;
+        System.out.println("int = " + num);
+        System.out.println("Integer = " + numWrapper);
+
+        byte num2 = 10;
+        Byte num2Wrapper = num2;
+        System.out.println("byte = " + num2);
+        System.out.println("Byte = " + num2Wrapper);
+    }
+
+}
